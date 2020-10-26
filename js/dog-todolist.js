e7a53d669ada394c7e027f3f09e502bfacb656a4
MingJing <a0911077842@gmail.com>
2020-10-26 13:13:32 +0800

    fixed more data

--- js/dog-todolist.js
+++ js/dog-todolist.js
@@ -3,10 +3,9 @@
  //指定Dom
  var list = document.querySelector('.my-list');
  var send = document.querySelector('.send');
- var data = JSON.parse(localStorage.getItem('listData')) || [];
-	console.log('list ='+list); 
+ var data = JSON.parse(localStorage.getItem('listData')) || []; 
 
-  send.addEventListener('click',addData , false);
+  send.addEventListener('click',addData);
   list.addEventListener('click', toggleDelete);
 
 
@@ -14,29 +13,19 @@ updataList(data);
 function updataList(data)
 {
   str ='';
-  console.log('data.length ='+ data.length);
   for(var i =0; i<data.length; i++)
   {
-    //str += '<li> <a href="#" data-index=' + i + ' />刪除</a>'+data[i].content + '</span></li>';
-    //str += '<li class ="list-group-item" > <a href="#" data-index=' + i + ' />刪除</a>'+data[i].content+'</li>';
-    //str += `<li class ="list-group-item"> <a href="#" data-index= ${i}>刪除</a>${data[i].content}</span></li>`;
     str +=`<li class ="list-group-item li-bg">
               <button  data-index= ${i} type="button" class="delete-btn send send-bg clearfix" >
                       <img src="image/icons8-cat-footprint-24.png" class="img-left">
-                      <p>Delete!</p>
+                      Delete!
                   </button>
                   ${data[i].content}
               </li>`
-
-    /// str += '<li class ="list-group-item" data-index = '+i+'>'+data[i].content+
-				// 	'<button type="button" class="delete-btn delete-bg clearfix">'+
-			 //          	'<img src="image/icons8-cat-footprint-24.png" class="img-left"><p>Delete!</p>'
-			 //        +'</button>'
-				// 	+'</li>;'
   }
   list.innerHTML = str; //UpdataContent
-  console.log('Data_array ='+data+ 'data type ='+typeof(data));
-  console.log('Data_string ='+JSON.stringify(data)+'data_string type ='+typeof(JSON.stringify(data)));
+  // console.log('Data_array ='+data+ 'data type ='+typeof(data));
+  // console.log('Data_string ='+JSON.stringify(data)+'data_string type ='+typeof(JSON.stringify(data)));
   
 }
 
@@ -45,11 +34,11 @@ function updataList(data)
   function addData(e)
   {
     e.preventDefault();
-    console.log('e.target.nodeName ='+e.target.nodeName)
+    // console.log('e.target.nodeName ='+e.target.nodeName)
     if(e.target.nodeName !== 'P'){return}//防止點錯
       
     var txt = document.querySelector('.text').value;
-    console.log('txt ='+txt);
+    // console.log('txt ='+txt);
     var todo = {content:txt};
     data.push(todo);//新增資料到物件
     localStorage.setItem('listData', JSON.stringify(data));
@@ -61,10 +50,10 @@ function updataList(data)
  function toggleDelete(e)
  {
   e.preventDefault();
-  console.log("delete :e.target.nodeName ="+e.target.nodeName);
-  if(e.target.nodeName !== 'P'){return}
+  // console.log("delete :e.target.nodeName ="+e.target.nodeName);
+  if(e.target.nodeName !== 'BUTTON'){return}
     var index = e.target.dataset.index;
-    console.log('e.target.dataset.index ='+index);
+    console.log('e.target.dataset.num ='+index);
     data.splice(index,1);
     localStorage.setItem('listData', JSON.stringify(data));//存資料時只能存string所以需要轉檔
     updataList(data);
